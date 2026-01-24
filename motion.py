b6a96ad61f75883ddf90464ac2af4a7e436b650c
Tyler Rozwadowski <tyroz523@gmail.com>
2026-01-24 01:07:13 -0600

    Revert "Tweak motion mask"
    This reverts commit 8e6081e42bef8bbfc68bed4547d5f2f6fcea42ef.

--- motion.py
+++ motion.py
@@ -6,17 +6,17 @@ class MotionDetector:
         self.motion_threshold = motion_threshold
         self.avg_history = deque(maxlen=moving_avg_frames)
         self.bg_subtractor = cv2.createBackgroundSubtractorMOG2(
-            history=500, varThreshold=40, detectShadows=False
+            history=500, varThreshold=16, detectShadows=False
         )
         self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
 
     def detect(self, roi_frame, freeze_bg=False):
-        lr = 0 if freeze_bg else -1
+        lr = 0 if freeze_bg else 0.001
         mask = self.bg_subtractor.apply(roi_frame, learningRate=lr)
         _, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, self.kernel, iterations=2)
-        mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_RECT, (7,3)), iterations=1)
+        mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_RECT, (15,3)), iterations=1)
 
         motion_area = cv2.countNonZero(mask)
         self.avg_history.append(motion_area)
